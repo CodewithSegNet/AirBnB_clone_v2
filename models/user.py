9abdc3a61d3f357091a2715dc7a790cd92774d47
WSL_Segun <eeomo@yahoo.com>
2023-03-25 19:04:01 -0700

    change files

--- models/user.py
+++ models/user.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
 from models.base_model import BaseModel, Base
-from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    '''
+        Definition of the User class
+    '''
+    __tablename__ = "users"
+    if getenv("new_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+
+        places = relationship('Place', cascade='all, delete-orphan',
+                              backref='user')
+        reviews = relationship('Review', cascade='all, delete-orphan',
+                               backref='user')
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
