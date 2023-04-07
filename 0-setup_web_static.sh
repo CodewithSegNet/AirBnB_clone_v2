3325a15aa011ec7518661bb0dc411b26dc1fa984
ubun_segun <eeomo@yahoo.com>
2023-04-07 02:01:59 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,17 +6,20 @@ sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 
+# create index.html for test directory
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 
-# Update Nginx configuration
-sudo sed -i 's#location \/ \{#location \/hbnb_static\/ \{#g' /etc/nginx/sites-available/default
-
-sudo sed -i 's#alias \/data\/web_static\/html\/\;#alias \/data\/web_static\/current\/\;\n\tindex index.html\;#g' /etc/nginx/sites-available/default
-
+# create symbolic link
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
+# give user ownership to directory
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '39 i\   \tlocation /new_static {\n\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
+# backup default server config file
+sudo cp /etc/nginx/sites-enabled/default nginx-sites-enabled_default.backup
+
+# Set-up the content of /data/web_static/current/ to redirect
+# to domain.tech/hbnb_static
+sudo sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
