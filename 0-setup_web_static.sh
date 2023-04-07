c8b9ff8ff4d0cb994397cf83d12fbf5285c876f5
ubun_segun <eeomo@yahoo.com>
2023-04-07 01:49:55 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,11 @@ sudo mkdir -p /data/web_static/shared
 
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 
+# Update Nginx configuration
+sudo sed -i 's#location \/ \{#location \/hbnb_static\/ \{#g' /etc/nginx/sites-available/default
+
+sudo sed -i 's#alias \/data\/web_static\/html\/\;#alias \/data\/web_static\/current\/\;\n\tindex index.html\;#g' /etc/nginx/sites-available/default
+
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
